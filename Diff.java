aaf490f0ebbf57c34e18a1093347c74b2510ab98
Chanakya-dev <Chanakyamanas123@gmail.com>
2024-08-11 21:41:07 +0530

    Create Diff.java

---
+++ Diff.java
@@ -0,0 +1,5 @@
+public class Diff{
+// Differentitae Synchronize static and Sunchronize with an Example 
+  // Run it here by creating Thread Class Object
+  // Diffrentiate Method Level Lock and Class Level Lock
+}
