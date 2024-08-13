e52c6f79772f73134159c3e75932f1a4d60ce66b
siva-245711 <sivanagendra809@gmail.com>
2024-08-13 20:50:28 +0530

    threads

--- Diff.java
+++ Diff.java
@@ -1,5 +1,84 @@
-public class Diff{
-// Differentitae Synchronize static and Sunchronize with an Example 
-  // Run it here by creating Thread Class Object
-  // Diffrentiate Method Level Lock and Class Level Lock
+public class Diff {
+  // Class-level lock
+  private static final Object classLock = new Object();
+
+  // Instance-level lock
+  private final Object instanceLock = new Object();
+
+  // Static synchronized method (class-level lock)
+  public static synchronized void staticSynchronizedMethod() {
+      System.out.println("Static synchronized method");
+      try {
+          Thread.sleep(1000);
+      } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+      }
+      System.out.println("Static synchronized method finished");
+  }
+
+  // Synchronized method with instance-level lock
+  public synchronized void instanceSynchronizedMethod() {
+      System.out.println("Instance synchronized method");
+      try {
+          Thread.sleep(1000);
+      } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+      }
+      System.out.println("Instance synchronized method finished");
+  }
+
+  // Synchronized block with class-level lock
+  public void classLevelLock() {
+      synchronized (classLock) {
+          System.out.println("Class-level lock");
+          try {
+              Thread.sleep(1000);
+          } catch (InterruptedException e) {
+              Thread.currentThread().interrupt();
+          }
+          System.out.println("Class-level lock finished");
+      }
+  }
+
+  // Synchronized block with instance-level lock
+  public void instanceLevelLock() {
+      synchronized (instanceLock) {
+          System.out.println("Instance-level lock");
+          try {
+              Thread.sleep(1000);
+          } catch (InterruptedException e) {
+              Thread.currentThread().interrupt();
+          }
+          System.out.println("Instance-level lock finished");
+      }
+  }
+
+  public static void main(String[] args) {
+      Diff diff1 = new Diff();
+      Diff diff2 = new Diff();
+
+      // Test static synchronized method
+      Thread thread1 = new Thread(Diff::staticSynchronizedMethod);
+      Thread thread2 = new Thread(Diff::staticSynchronizedMethod);
+      thread1.start();
+      thread2.start();
+
+      // Test instance synchronized method
+      Thread thread3 = new Thread(() -> diff1.instanceSynchronizedMethod());
+      Thread thread4 = new Thread(() -> diff2.instanceSynchronizedMethod());
+      thread3.start();
+      thread4.start();
+
+      // Test class-level lock
+      Thread thread5 = new Thread(() -> diff1.classLevelLock());
+      Thread thread6 = new Thread(() -> diff2.classLevelLock());
+      thread5.start();
+      thread6.start();
+
+      // Test instance-level lock
+      Thread thread7 = new Thread(() -> diff1.instanceLevelLock());
+      Thread thread8 = new Thread(() -> diff2.instanceLevelLock());
+      thread7.start();
+      thread8.start();
+  }
 }
--- YieldnJoin.java
+++ YieldnJoin.java
@@ -1,3 +1,45 @@
-public class YieldnJoin{
-// Write Your Code Here
+class MyThread extends Thread {
+    public MyThread(String name) {
+        super(name);
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println(Thread.currentThread().getName() + " - " + i);
+            // Yield control to other threads
+            Thread.yield();
+        }
+    }
 }
+
+public class YieldnJoin {
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread("Thread-1");
+        MyThread t2 = new MyThread("Thread-2");
+        MyThread t3 = new MyThread("Thread-3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        try {
+            // Ensure t1 completes before proceeding
+            t1.join();
+            System.out.println(t1.getName() + " has finished.");
+
+            // Ensure t2 completes before proceeding
+            t2.join();
+            System.out.println(t2.getName() + " has finished.");
+
+            // Ensure t3 completes before proceeding
+            t3.join();
+            System.out.println(t3.getName() + " has finished.");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All threads have finished execution.");
+    }
+}
+
