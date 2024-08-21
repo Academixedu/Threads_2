bd1330af34673c853c53c66d44d9ace5959634de
psvsai <pasupuletisvsm245@gmail.com>
2024-08-21 20:27:18 +0530

    PSVSAI

--- Diff.java
+++ Diff.java
@@ -2,4 +2,29 @@ public class Diff{
 // Differentitae Synchronize static and Sunchronize with an Example 
   // Run it here by creating Thread Class Object
   // Diffrentiate Method Level Lock and Class Level Lock
+  public synchronized void a1(){
+    for (int i=1; i<=8; i++){
+      System.out.println("Static:1 " +" "+ Thread.currentThread().getName());
+    }
+  }
+  public static synchronized void a2(){
+    for(int i=1; i<=8; i++){
+      System.out.println("STatic:2 "+ " " + Thread.currentThread().getName());
+    }
+  }
+  public static void main(String[] args) {
+    Diff d = new Diff();
+    Thread t = new Thread(){
+      public void run(){
+        d.a1();
+      }
+    };t.start();
+  
+  Diff d1 = new Diff();
+  Thread t1 = new Thread(){
+    public void run(){
+      d1.a2();
+    }
+  };t1.start();
+}
 }
--- Test.java
+++ Test.java
@@ -1,4 +1,24 @@
 public class Test{
 // Create Thread Here and Test it Here of Logic Written in Example File
   // Apply Thread Class Priority() Method
+  public static void main(String[] args) {
+    Example e = new Example();
+    Thread t = new Thread()
+    {
+      public void run(){
+        e.deposit(25000);
+      }
+    };
+    t.setPriority(6);
+    t.start();
+
+    Thread t1=new Thread(){
+      public void run(){  
+  e.withdrawl(5600);
+  }
+  };
+  t1.setPriority(10);
+  t1.start();
+
+  }
 }
