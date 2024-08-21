62833b4b1333e0ab8e25ae5bb82145b79bcf3384
preetham0223 <sagilinoyal@gmail.com>
2024-08-22 00:12:06 +0530

    commit

--- Diff.java
+++ Diff.java
@@ -1,5 +1,51 @@
-public class Diff{
-// Differentitae Synchronize static and Sunchronize with an Example 
-  // Run it here by creating Thread Class Object
-  // Diffrentiate Method Level Lock and Class Level Lock
+public class Diff {
+
+  public synchronized void m1() {
+      for (int i = 1; i <= 5; i++) {
+          System.out.println("Instance " + Thread.currentThread().getName());
+      }
+  }
+
+  public static synchronized void m2() {
+      for (int i = 1; i <= 5; i++) {
+          System.out.println("Static " + Thread.currentThread().getName());
+      }
+  }
+
+  public static void main(String[] args) {
+      // Creating first instance
+      Diff s = new Diff();
+      Thread t1 = new Thread() {
+          public void run() {
+              s.m1(); 
+          }
+      };
+
+    
+      Diff s1 = new Diff();
+      Thread t2 = new Thread() {
+          public void run() {
+              s1.m1();
+          }
+      };
+
+      
+      Thread t3 = new Thread() {
+          public void run() {
+              Diff.m2();
+          }
+      };
+
+      Thread t4 = new Thread() {
+          public void run() {
+              Diff.m2(); 
+          }
+      };
+
+   
+      t1.start();
+      t2.start();
+      t3.start();
+      t4.start();
+  }
 }
