3611a747ae386d29f63c76219e54dfdae9e71c09
sujatha-94 <sujathakarri701@gmail.com>
2024-08-21 18:15:41 +0530

    Threads

--- DeadLock.java
+++ DeadLock.java
@@ -1,3 +1,46 @@
 public class DeadLock{
 // Write Your Code Here
+public static void main(String[] args) {
+    Resource r1 = new Resource();
+    Resource r2 = new Resource();
+
+    Thread thread1 = new Thread(() -> {
+        synchronized (r1) {
+            System.out.println("Thread 1: Locked resource 1");
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 1: Trying to lock resource 2");
+            synchronized (r2) {
+                System.out.println("Thread 1: Locked resource 2");
+            }
+        }
+    });
+
+    Thread thread2 = new Thread(() -> {
+        synchronized (r2) {
+            System.out.println("Thread 2: Locked resource 2");
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Thread 2: Trying to lock resource 1");
+            synchronized (r1) {
+                System.out.println("Thread 2: Locked resource 1");
+            }
+        }
+    });
+
+    thread1.start();
+    thread2.start();
+}
+}
+class Resource {
+    public synchronized void action(Resource otherResource) {
+        System.out.println(Thread.currentThread().getName() + ": Holding " + this + " and waiting for " + otherResource);
+        otherResource.action(this); // Try to acquire lock on the otherResource
+    }
 }
