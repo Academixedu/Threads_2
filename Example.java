fe468e05fd5d40dee2c950b07474040c20728e22
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-08-21 19:07:33 +0530

    thredss

--- Example.java
+++ Example.java
@@ -1,3 +1,24 @@
 public class Example{
 // Preapare an Example for Inter Thread Communincation use wait() and Notify() Methods
+public static double balance=50000;
+    public synchronized  void deposit(double deposite){
+        balance=balance+deposite;
+        System.out.println("Deposit : "+deposite);
+        System.out.println("Balance : "+balance);
+        notify();
+    }
+    public synchronized void withdrawl(double withdraw){
+    if(balance<=withdraw){
+        try {
+            wait(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }    
+    else if(balance>withdraw){   
+    balance=balance-withdraw;
+    System.out.println("Withdrawl : "+withdraw);
+    System.out.println("Balance : "+balance);
+}}
+    
 }
--- Test.java
+++ Test.java
@@ -1,4 +1,25 @@
 public class Test{
 // Create Thread Here and Test it Here of Logic Written in Example File
   // Apply Thread Class Priority() Method
+
+  public static void main(String[] args) {
+    Example e = new Example();
+    Thread t = new Thread()
+    {
+      public void run(){
+        e.deposit(2500);
+      }
+    };
+    t.setPriority(6);
+    t.start();
+
+    Thread t1=new Thread(){
+      public void run(){  
+  e.withdrawl(560);
+  }
+  };
+  t1.setPriority(10);
+  t1.start();
+
+  }
 }
