8e124639f006d8d37013d3e511dc7ae3e4baa1ef
nayeem6836 <maseednayeem@gmail.com>
2024-08-21 17:07:45 +0530

    Update Diff.java

--- Diff.java
+++ Diff.java
@@ -1,13 +1,11 @@
 public class Diff {
 
-    // Instance-level synchronized method
     public synchronized void m1() {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Instance " + Thread.currentThread().getName());
         }
     }
 
-    // Class-level synchronized method
     public static synchronized void m2() {
         for (int i = 1; i <= 5; i++) {
             System.out.println("Static " + Thread.currentThread().getName());
@@ -19,32 +17,32 @@ public class Diff {
         Diff s = new Diff();
         Thread t1 = new Thread() {
             public void run() {
-                s.m1(); // Instance-level lock
+                s.m1(); 
             }
         };
 
-        // Creating second instance
+      
         Diff s1 = new Diff();
         Thread t2 = new Thread() {
             public void run() {
-                s1.m1(); // Instance-level lock
+                s1.m1();
             }
         };
 
-        // Static method calls
+        
         Thread t3 = new Thread() {
             public void run() {
-                Diff.m2(); // Class-level lock
+                Diff.m2();
             }
         };
 
         Thread t4 = new Thread() {
             public void run() {
-                Diff.m2(); // Class-level lock
+                Diff.m2(); 
             }
         };
 
-        // Start all threads
+     
         t1.start();
         t2.start();
         t3.start();
